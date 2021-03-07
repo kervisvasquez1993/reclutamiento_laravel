0405adfeb6ea1842ca166045cdb2acf920a26cde
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-07 11:02:06 -0400

    componente de vue para cambiar el estado

---
+++ resources/js/components/EstadoVacante.vue
@@ -0,0 +1,35 @@
+<template>
+    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+         @click="cambiarEstado"
+         :key="estadoVacanteData"
+         :class="claseEstadoVacante()"
+    >
+                    Activa
+    </span>
+</template>
+<script>
+export default {
+    props : ['estado', 'vacanteId'],
+    mounted() 
+    {
+        this.estadoVacanteData = Number(this.estado)
+    },
+    data : function()
+    {
+        return {
+            estadoVacanteData : null
+        }
+    },
+    methods: {
+       
+        claseEstadoVacante()
+        {
+            return this.estadoVacanteData === 1 ? "bg-green-100 text-green-800" : "bg-red-100 text-green-800" 
+        },
+       cambiarEstado()
+        {
+
+        } 
+    },
+}
+</script>
