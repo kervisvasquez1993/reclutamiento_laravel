94b8f8f9a8ded5cfd126a4bfcc4abd84acef39d9
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-07 18:37:34 -0400

    agregamos componente para cambiar estaus desde vue y agregamos componente para eliminar

---
+++ resources/js/components/EliminarVacante.vue
@@ -0,0 +1,16 @@
+<template>
+    <button  class="text-red-600 hover:text-red-900  mr-5"
+    @click="eliminarVacante"
+    >Eliminar</button>
+</template>
+<script>
+export default {
+    props : ['vacanteId'],
+    methods: {
+        eliminarVacante()
+        {
+            console.log('vacante eliminada', this.vacanteId)
+        }
+    },
+}
+</script>
